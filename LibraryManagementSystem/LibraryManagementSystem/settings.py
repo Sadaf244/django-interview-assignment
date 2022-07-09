a6e7ca6828e5e5debfcc72542f54ca947940513d
Sadaf244 <sadafimukhtar@gmail.com>
2022-07-09 13:51:49 +0530

    Some modification

--- LibraryManagementSystem/LibraryManagementSystem/settings.py
+++ LibraryManagementSystem/LibraryManagementSystem/settings.py
@@ -15,8 +15,10 @@ from datetime import timedelta
 import os
 import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
