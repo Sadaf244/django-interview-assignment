2ff72b8ed8658aeaeab6d3649cf885def9a992cc
Sadaf244 <sadafimukhtar@gmail.com>
2022-07-09 14:24:49 +0530

    Modified Setting.py

--- LibraryManagementSystem/LibraryManagementSystem/settings.py
+++ LibraryManagementSystem/LibraryManagementSystem/settings.py
@@ -137,12 +137,12 @@ USE_TZ = True
 # STATIC_URL = 'static/'
 # STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_URL = 'static/'
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT =os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 django_heroku.settings(locals())
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 REST_FRAMEWORK = {
