b99b21fdba77460f442bbef499211bab9abf1575
Sadaf244 <sadafimukhtar@gmail.com>
2022-07-09 12:16:45 +0530

    change file directory

--- LibraryManagementSystem/LibraryManagementSystem/settings.py
+++ LibraryManagementSystem/LibraryManagementSystem/settings.py
@@ -132,8 +132,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 # STATIC_URL = 'static/'
-STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
+# STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = 'static/'
 django_heroku.settings(locals())
 
 # Default primary key field type
